ffe4b029315b4d4c053f2d7dc7bc9b1dae5fdf4c
François <franois.mahy28@gmail.com>
2024-09-23 22:04:34 +0200

    v1 front

--- src/pages/Camera/Camera.tsx
+++ src/pages/Camera/Camera.tsx
@@ -15,12 +15,44 @@ const Camera = () => {
             <div className="flex h-[35vh] gap-8 pt-8">
 
                 <div className="w-2/3 bg-secondary-color rounded-xl p-4 flex justify-between">
-                    <div className="bg-red-500 w-1/3 border-2 border-green-500 rounded-xl overflow-hidden">
+                    <div className="w-1/3 border-2 border-green-500 rounded-xl overflow-hidden mr-4">
                         <MapComponent/>
                     </div>
 
-                    <div className="border-x-2 border-green-500 px-4 w-1/3 mx-4">
+                    <div className="border-r-2 border-green-500 pr-12 w-1/3 flex items-center justify-center">
+                        <div className=" mx-4 flex items-center w-1/2">
+                            <dl className="w-full text-white divide-y divide-gray-500">
+                                <div className="flex flex-col pb-3">
+                                    <dt className="mb-1  md:text-lg font-semibold">IP Address</dt>
+                                    <dd className="">192.168.0.16</dd>
+                                </div>
+                                <div className="flex flex-col py-3">
+                                    <dt className="mb-1  md:text-lg font-semibold">Version</dt>
+                                    <dd className="">V 0.01</dd>
+                                </div>
+                                <div className="flex flex-col py-3">
+                                    <dt className="mb-1  md:text-lg font-semibold">Version</dt>
+                                    <dd className="">V 0.01</dd>
+                                </div>
+                            </dl>
+                        </div>
 
+                        <div className="mx-4 flex items-center w-1/2">
+                            <dl className="w-full text-white divide-y divide-gray-500">
+                                <div className="flex flex-col pb-3">
+                                    <dt className="mb-1  md:text-lg font-semibold">IP Address</dt>
+                                    <dd className="">192.168.0.16</dd>
+                                </div>
+                                <div className="flex flex-col py-3">
+                                    <dt className="mb-1  md:text-lg font-semibold">Version</dt>
+                                    <dd className="">V 0.01</dd>
+                                </div>
+                                <div className="flex flex-col py-3">
+                                    <dt className="mb-1  md:text-lg font-semibold">Version</dt>
+                                    <dd className="">V 0.01</dd>
+                                </div>
+                            </dl>
+                        </div>
                     </div>
 
                     <div className="w-1/3 h-full flex items-center justify-center">
@@ -29,6 +61,18 @@ const Camera = () => {
                 </div>
 
                 <div className="w-1/3 bg-secondary-color rounded-xl ">
+                    <div className="w-full h-1/6 flex items-center justify-center">
+                        <h3 className="text-gray-300 font-semibold text-xl pr-4 border-r-2">Heisenberg</h3>
+                        <span className="pl-4 text-gray-300 font-semibold text-xl">23/09/2023 22:03</span>
+                    </div>
+
+                    <div className="h-5/6">
+                        <img
+                            src="https://dessin-creation.com/wp-content/uploads/2014/11/comment-dessiner-un-visage-walter-white.jpg"
+                            className="w-full h-full object-cover"
+                            alt="Walter White"
+                        />
+                    </div>
 
                 </div>
             </div>
--- src/pages/Camera/CameraSettings/CameraSettings.tsx
+++ src/pages/Camera/CameraSettings/CameraSettings.tsx
@@ -1,28 +1,36 @@
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faBell} from "@fortawesome/free-solid-svg-icons";
+import {faBell, faUser, faCat, faGun} from "@fortawesome/free-solid-svg-icons";
 import Camera from "../../../../public/surveillance.png"
 import {useUpdateAlarmStatus} from "../../../api/Queries/CameraQueries.ts";
+import {useState} from "react";
+import DetectionItem from "./DetectionItem.tsx";
 
 const CameraSettings = () => {
     const {mutate: mutateUpdateChanneName} = useUpdateAlarmStatus();
-
+    const [rangeValue, setRangeValue] = useState(50);
 
     const handleUpdateAlarmStatus= () => {
         mutateUpdateChanneName(
             true,
             {
                 onSuccess: () => {
-                    console.log("OK")
+
                 },
-                onError: (error) => {
-                    console.error("Error resetting password:", error.message);
+                onError: () => {
+
                 },
             }
         );
     }
 
+
+    const handleRangeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setRangeValue(Number(e.target.value));
+    };
+
     return (
-        <div className="w-1/3 bg-secondary-color gap-8 rounded-xl px-6 py-8 flex flex-col">
+        <div className="w-1/3 bg-secondary-color gap-8 rounded-xl px-6 py-8 flex flex-col justify-around">
+
             <div className="flex justify-between border-b-2 pb-4 border-green-500">
                 <div className="pr-6">
                     <h2 className="uppercase text-white text-4xl font-bold">Caméra salon</h2>
@@ -33,13 +41,40 @@ const CameraSettings = () => {
                 <img src={Camera} className="w-16 h-16"/>
             </div>
 
+            <div className="border-b-2 pb-4 border-green-500">
+                <h3 className="mb-2 text-xl text-white font-semibold">Sensivity</h3>
+                <input
+                    type="range"
+                    min={0}
+                    max="100"
+                    value={rangeValue}
+                    onChange={handleRangeChange}
+                    className="range"
+                    step="25"
+                />
+                <div className="flex w-full justify-between px-2 text-xs">
+                    <span>1</span>
+                    <span>2</span>
+                    <span>3</span>
+                    <span>4</span>
+                    <span>5</span>
+                </div>
+            </div>
+
+            <div className="border-b-2 pb-4 border-green-500">
+                <h3 className="mb-2 text-xl text-white font-semibold">Detection</h3>
+
+                <ul className="divide-y divide-gray-200 dark:divide-gray-700 w-full">
+                    <DetectionItem icon={faUser} title={"Person"} isActive={false}  />
+                    <DetectionItem icon={faCat} title={"Animal"} isActive={false}  />
+                    <DetectionItem icon={faGun} title={"Weapon"} isActive={false}  />
+                </ul>
+
+            </div>
+
             <div className={"flex justify-center"}>
                 <FontAwesomeIcon onClick={handleUpdateAlarmStatus} icon={faBell}
-                                 className="text-white bg-third-color rounded-full p-3.5 text-xl my-2.5 hover:bg-green-500"/>
-                <FontAwesomeIcon icon={faBell}
-                                 className="text-white bg-third-color rounded-full p-3.5 text-xl my-2.5 mx-4 hover:bg-green-500"/>
-                <FontAwesomeIcon icon={faBell}
-                                 className="text-white bg-third-color rounded-full p-3.5 text-xl my-2.5 hover:bg-green-500"/>
+                                 className="cursor-pointer text-white bg-third-color rounded-full p-3.5 text-xl my-2.5 hover:bg-green-500"/>
             </div>
         </div>
     )
